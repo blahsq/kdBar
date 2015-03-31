3b928c9df2d39f517921b4b92ff1f71821637a9f
Krzysztof Dalek <krzysztof.dalek@grandparade.co.uk>
2015-03-31 17:55:35 +0200

    Initial commit

---
+++ kdbar.js
@@ -0,0 +1,23 @@
+(function ($) {
+     $.fn.kdBar = function(options) {
+         var _this = this,
+             settings = $.extend({
+             backgroundColor: 'green',
+             height: '5'
+         }, options );
+         
+         _this.css({
+             'position': 'fixed',
+             'height': settings.height + 'px'
+         });
+         $(window).on('scroll', function() {
+             var windowScrollTop = $(window).scrollTop(),
+             documentHeight = $(document).height(),
+             windowHeight = $(window).height(),
+             progressPosition = (windowScrollTop / (documentHeight - windowHeight)) * 100;
+    
+             _this.css('width', progressPosition + '%');
+         });
+    };
+ 
+} (jQuery));
